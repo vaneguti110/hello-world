63afcc921e338293bf7ccd4f1c3c34b5c717ba20
Vanessa <vaneguti110@hotmail.com>
2016-08-25 13:10:43 +0200

    files to try some commands using python

---
+++ # code Window.py
@@ -0,0 +1,12 @@
+# code Window
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print "String: %s" % mystring
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print "Float: %d" % myfloat
+if isinstance(myint, int) and myint == 20:
+    print "Integer: %d" % myint
---
+++ #code Output window from the exercise previously.py
@@ -0,0 +1,5 @@
+#code Output window from the exercise previously
+
+String: hello
+Float: 10
+Integer: 20
